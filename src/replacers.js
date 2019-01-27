e4acae928a463d5b23f8010e559f360d8d6fa7a2
patfb <>
2019-01-27 01:22:21 -0500

    adding methods that I'll need

--- src/replacers.js
+++ src/replacers.js
@@ -1,8 +1,37 @@
-module.exports = {
-	replacePeriods: function(input) {
-		return input.replace(".", " ");
-	},
-	replaceDashes: function(input) {
-		return input.replace("-", " ");
+let replacePeriods = (input) => {
+	return input.replace(".", " ");
+};
+
+let replaceDashes = (input) => {
+	return input.replace("-", " ");
+};
+
+let separateExtension = (input) => {
+	return input.slice(input.lastIndexOf("."));
+};
+
+let onlyFileName = (input) => {
+	return input.slice(0, input.lastIndexOf("."));
+};
+
+let convertToFilenameObject = (originalFilenames) => {
+	let mappedFilenames = [];
+
+	for (const file of originalFilenames) {
+		let filename = {
+			original: onlyFileName(file),
+			renamed: null,
+			extension: separateExtension(file)
+		};
+		mappedFilenames.push(filename);
 	}
+	return mappedFilenames;
+};
+
+module.exports = {
+	replacePeriods,
+	replaceDashes,
+	separateExtension,
+	onlyFileName,
+	convertToFilenameObject
 };
--- test/replace.spec.js
+++ test/replace.spec.js
@@ -4,6 +4,36 @@ r = require("../src/replacers");
 describe("replacers", function() {
 	const unchanged = "unchanged";
 
+	describe("convertToFilenameObject()", function() {
+		describe("when given an array of filenames", function() {
+			it("should return an array of filename objects", function() {
+				const original = ["file1.txt", "file2.txt"];
+				const expected = [
+					{ original: "file1", renamed: null, extension: ".txt" },
+					{ original: "file2", renamed: null, extension: ".txt" }
+				];
+
+				assert.deepEqual(expected, r.convertToFilenameObject(original));
+			});
+		});
+	});
+
+	describe("onlyFileName()", function() {
+		describe("when given a file", function() {
+			it("should return the file name without the extension", function() {
+				assert.equal("something", r.onlyFileName("something.mkv"));
+			});
+		});
+	});
+
+	describe("separateExtension()", function() {
+		describe("when given a file", function() {
+			it("should return the extension", function() {
+				assert.equal(".mkv", r.separateExtension("something.mkv"));
+			});
+		});
+	});
+
 	describe("replaceDashes()", function() {
 		describe("when dash is present", function() {
 			it("should replace dash", function() {
@@ -16,6 +46,7 @@ describe("replacers", function() {
 			});
 		});
 	});
+
 	describe("replacePeriods()", function() {
 		describe("when period is present", function() {
 			it("should replace periods", function() {
