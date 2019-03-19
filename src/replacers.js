8c62f6795e6e7c4b5ad02712e58799c6fa6e2993
patfb <>
2019-03-18 21:34:09 -0400

    adding default prettier config. updating tests. add npm scripts for linting and testing

--- src/replacers.js
+++ src/replacers.js
@@ -1,45 +1,45 @@
-let separateExtension = (input) => {
-	return input.slice(input.lastIndexOf("."));
-};
+let separateExtension = input => {
+  return input.slice(input.lastIndexOf("."))
+}
 
-let onlyFileName = (input) => {
-	return input.slice(0, input.lastIndexOf("."));
-};
+let separateFileName = input => {
+  return input.slice(0, input.lastIndexOf("."))
+}
 
-let convertToFileNameObject = (originalFileName) => {
-	return {
-		name: onlyFileName(originalFileName),
-		renamed: null,
-		extension: separateExtension(originalFileName)
-	};
-};
+let convertToFileNameObject = originalFileName => {
+  return {
+    name: separateFileName(originalFileName),
+    renamed: null,
+    extension: separateExtension(originalFileName),
+  }
+}
 
-let twoDigitFormatter = (input) => {
-	if (input.length <= 2) {
-		return ("0" + input).slice(-2);
-	} else {
-		return input;
-	}
-};
+let twoDigitFormatter = input => {
+  if (input.length <= 2) {
+    return ("0" + input).slice(-2)
+  } else {
+    return input
+  }
+}
 
 let rename = (fileNameObjects, baseName, startingNumber) => {
-	let startingNumberAsNumber = startingNumber;
-	let renamedArray = [];
-	for (const file of fileNameObjects) {
-		file.renamed = `${baseName} ${twoDigitFormatter(
-			startingNumberAsNumber.toString()
-		)}`;
+  let startingNumberAsNumber = startingNumber
+  let renamedArray = []
+  for (const file of fileNameObjects) {
+    file.renamed = `${baseName} ${twoDigitFormatter(
+      startingNumberAsNumber.toString()
+    )}`
 
-		startingNumberAsNumber++;
-		renamedArray.push(file);
-	}
-	return renamedArray;
-};
+    startingNumberAsNumber++
+    renamedArray.push(file)
+  }
+  return renamedArray
+}
 
 module.exports = {
-	separateExtension,
-	onlyFileName,
-	convertToFileNameObject,
-	twoDigitFormatter,
-	rename
-};
+  separateExtension,
+  separateFileName: separateFileName,
+  convertToFileNameObject,
+  twoDigitFormatter,
+  rename,
+}
--- test/replacers.spec.js
+++ test/replacers.spec.js
@@ -1,88 +1,76 @@
-const assert = require("chai").assert;
-r = require("../src/replacers");
+const assert = require("chai").assert
+r = require("../src/replacers")
 
 describe("replacers", function() {
-	describe("rename()", function() {
-		describe("when given an array of filename objects", function() {
-			it("should populate the rename field with new file name", function() {
-				let original = [
-					{ name: "text", renamed: null, extension: ".txt" },
-					{ name: "text", renamed: null, extension: ".txt" },
-					{ name: "text", renamed: null, extension: ".txt" }
-				];
+  describe("rename()", function() {
+    describe("when given an array of filename objects", function() {
+      it("should populate the rename field with new file name", function() {
+        let original = [
+          { name: "first", renamed: null, extension: ".txt" },
+          { name: "second", renamed: null, extension: ".txt" },
+          { name: "third", renamed: null, extension: ".txt" },
+        ]
 
-				let expected = [
-					{
-						name: "text",
-						renamed: "New File Name S02E01",
-						extension: ".txt"
-					},
-					{
-						name: "text",
-						renamed: "New File Name S02E02",
-						extension: ".txt"
-					},
-					{
-						name: "text",
-						renamed: "New File Name S02E03",
-						extension: ".txt"
-					}
-				];
+        let expected = [
+          {
+            name: "first",
+            renamed: "New File Name 01",
+            extension: ".txt",
+          },
+          {
+            name: "second",
+            renamed: "New File Name 02",
+            extension: ".txt",
+          },
+          {
+            name: "third",
+            renamed: "New File Name 03",
+            extension: ".txt",
+          },
+        ]
 
-				assert.deepEqual(
-					expected,
-					r.rename(original, "New File Name", 2, 1)
-				);
-			});
-		});
-	});
+        assert.deepEqual(expected, r.rename(original, "New File Name", 1))
+      })
+    })
+  })
 
-	describe("twoDigitFormatter()", function() {
-		describe("when given a number", function() {
-			it("should return properly formatted number", function() {
-				assert.equal("01", r.twoDigitFormatter("1"));
-				assert.equal("12", r.twoDigitFormatter("12"));
-				assert.equal("100", r.twoDigitFormatter("100"));
-			});
-		});
-	});
+  describe("twoDigitFormatter()", function() {
+    describe("when given a number", function() {
+      it("should return properly formatted number", function() {
+        assert.equal("01", r.twoDigitFormatter("1"))
+        assert.equal("12", r.twoDigitFormatter("12"))
+        assert.equal("102", r.twoDigitFormatter("102"))
+      })
+    })
+  })
 
-	describe("rename()", function() {
-		describe("when given an array of fileNameObjects", function() {
-			it("should rename the original files and save them in the object", function() {});
-		});
-	});
+  describe("convertToFileNameObject()", function() {
+    describe("when given an array of filenames", function() {
+      it("should return an array of filename objects", function() {
+        const expected = {
+          name: "file1",
+          renamed: null,
+          extension: ".txt",
+        }
 
-	describe("convertToFileNameObject()", function() {
-		describe("when given an array of filenames", function() {
-			it("should return an array of filename objects", function() {
-				const expected = {
-					name: "file1",
-					renamed: null,
-					extension: ".txt"
-				};
+        assert.deepEqual(expected, r.convertToFileNameObject("file1.txt"))
+      })
+    })
+  })
 
-				assert.deepEqual(
-					expected,
-					r.convertToFileNameObject("file1.txt")
-				);
-			});
-		});
-	});
+  describe("separateFileName()", function() {
+    describe("when given a file", function() {
+      it("should return the file name without the extension", function() {
+        assert.equal("something", r.separateFileName("something.mkv"))
+      })
+    })
+  })
 
-	describe("onlyFileName()", function() {
-		describe("when given a file", function() {
-			it("should return the file name without the extension", function() {
-				assert.equal("something", r.onlyFileName("something.mkv"));
-			});
-		});
-	});
-
-	describe("separateExtension()", function() {
-		describe("when given a file", function() {
-			it("should return the extension", function() {
-				assert.equal(".mkv", r.separateExtension("something.mkv"));
-			});
-		});
-	});
-});
+  describe("separateExtension()", function() {
+    describe("when given a file", function() {
+      it("should return the extension", function() {
+        assert.equal(".mkv", r.separateExtension("something.mkv"))
+      })
+    })
+  })
+})
