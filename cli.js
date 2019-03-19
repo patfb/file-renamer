476be638405d75b2674f8cd20ed9fb9682eced8c
patfb <>
2019-03-18 23:32:33 -0400

    remove "preview" console. update prettier to work on root

--- cli.js
+++ cli.js
@@ -1,52 +1,51 @@
 #!/usr/bin/env node
 
-const fs = require("fs")
-const r = require("./src/replacers")
-const readlineSync = require("readline-sync")
-const naturalSort = require("./node_modules/javascript-natural-sort/naturalSort")
-let fileNames = []
+const fs = require("fs");
+const r = require("./src/replacers");
+const readlineSync = require("readline-sync");
+const naturalSort = require("./node_modules/javascript-natural-sort/naturalSort");
+let fileNames = [];
 
 // process.cwd() gets current working directory that script was invoked from
 
-const currentWorkingDirectory = process.cwd()
+const currentWorkingDirectory = process.cwd();
 
-let unsortedFiles = fs.readdirSync(process.cwd())
+let unsortedFiles = fs.readdirSync(process.cwd());
 
-let sortedFiles = unsortedFiles.sort(naturalSort)
+let sortedFiles = unsortedFiles.sort(naturalSort);
 
 sortedFiles.forEach(file => {
-  fileNameObject = r.convertToFileNameObject(file)
-  fileNames.push(fileNameObject)
-})
+  fileNameObject = r.convertToFileNameObject(file);
+  fileNames.push(fileNameObject);
+});
 
-console.log("Current files in this directory:")
+console.log("Current files in this directory:");
 for (const file of fileNames) {
-  console.log(`${file.name}${file.extension}`)
+  console.log(`${file.name}${file.extension}`);
 }
 
-const baseFileName = readlineSync.question("Base file name: ")
-const startingNumber = readlineSync.question("Starting number: ")
+const baseFileName = readlineSync.question("Base file name: ");
+const startingNumber = readlineSync.question("Starting number: ");
 
-let renamedFileNames = r.rename(fileNames, baseFileName, startingNumber)
+let renamedFileNames = r.rename(fileNames, baseFileName, startingNumber);
 
-console.log("Preview:")
-console.log("Directory:", currentWorkingDirectory)
+console.log("Directory:", currentWorkingDirectory);
 for (const file of renamedFileNames) {
   console.log(
     `${file.name}${file.extension} --> ${file.renamed}${file.extension}`
-  )
+  );
 }
 
 if (readlineSync.keyInYN("Continue with rename?")) {
   // 'Y' key was pressed.
-  console.log("Renaming files...")
+  console.log("Renaming files...");
   for (const file of renamedFileNames) {
     fs.renameSync(
       currentWorkingDirectory + "\\" + file.name + file.extension,
       currentWorkingDirectory + "\\" + file.renamed + file.extension
-    )
+    );
   }
 } else {
   // Another key was pressed.
-  console.log("Rename cancelled")
+  console.log("Rename cancelled");
 }
